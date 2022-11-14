4f1c77fe8cfa5b34ed076bf1b08911bfc42cc230
jagannath <jagannathanshikar@gmail.com>
2022-11-14 12:43:54 +0530

    search filter

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
+
+
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    filter_backends = [SearchFilter]
+    # search_fields = ['city', 'id', 'name']
+    search_fields = ['^name'] # search data by single character
+
+
+    
---
+++ readme.txt
@@ -0,0 +1,11 @@
+urls searching method 
+
+url/?search = data(provided in views.py file)
+
+
+changing search parameter
+
+REST_FRAMEWORK = {
+    'SEARCH_PARAM': 'q'
+}
+
